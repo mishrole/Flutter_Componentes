d2618e12df9e4a2625596ef80a71d8ef76b08d72
Mitchell Rodríguez <mitrodle@gmail.com>
2021-06-21 18:54:49 -0500

    [Add]: Card FadeInImage + Assets

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -10,7 +10,9 @@ class CardPage extends StatelessWidget {
         // padding: EdgeInsets.all(20),
         padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
         children: <Widget>[
-          _cardTipo1()
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
         ]
       ),
     );
@@ -37,5 +39,28 @@ Widget _cardTipo1() {
         mainAxisAlignment: MainAxisAlignment.end,
       )
     ],)
+  );
+}
+
+Widget _cardTipo2() {
+  return Card(
+    child: Column(
+      children: <Widget>[
+        FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage('https://th.bing.com/th/id/OIP.9ftNIxFdXhhw1jqMYwDhMQHaEo?pid=ImgDet&rs=1'),
+          fadeInDuration: Duration( milliseconds: 200 ),
+          height: 250.0,
+          fit: BoxFit.cover,
+          ),
+        /*
+        Image(
+          image: NetworkImage('https://th.bing.com/th/id/OIP.9ftNIxFdXhhw1jqMYwDhMQHaEo?pid=ImgDet&rs=1')
+        ),
+        */
+        Container(child: Text('Hola'),
+        padding: EdgeInsets.all(10.0),)
+      ],
+    )
   );
 }
