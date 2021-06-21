b61a5fe1ebfc882984c8372d79b3b7afeaab0f1f
Mitchell Rodríguez <mitrodle@gmail.com>
2021-06-21 16:59:00 -0500

    [Add]: Routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:componentes/src/pages/alert_page.dart';
-import 'package:componentes/src/pages/avatar_page.dart';
+import 'package:componentes/src/routes/routes.dart';
 import 'package:flutter/material.dart';
-import 'package:componentes/src/pages/home_page.dart';
  
 void main() => runApp(MyApp());
  
@@ -13,10 +12,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       //home: HomePage()
       initialRoute: '/',
-      routes: <String, WidgetBuilder> {
-        '/' : (BuildContext context) => HomePage(),
-        'alert' : (BuildContext context) => AlertPage(),
-        'avatar' : (BuildContext context) => AvatarPage(),
+      routes: getApplicationRoutes(),
+      onGenerateRoute: ( RouteSettings settings ) {
+        return MaterialPageRoute(builder: (BuildContext context) => AlertPage());
       },
     );
   }
