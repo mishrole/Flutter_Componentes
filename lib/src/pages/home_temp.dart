47fba465bb0a3c7a85a8a8f96dbdf61514f71701
Mitchell Rodríguez <mitrodle@gmail.com>
2021-06-21 13:08:59 -0500

    [Add]: For and Map

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,18 +1,45 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Componentes Temp'),),
       body: ListView(
-        children: <Widget>[
-          ListTile(title: Text('ListTitle Title'),),
-          Divider(),
-          ListTile(title: Text('ListTitle Title'),)
-        ],
+        // children: _crearItem()
+        children: _crearItemsCorta()
       ),
     );
+  }
+
+  List<Widget> _crearItem() {
+    List<Widget> lista = [];
+
+    for (String opcion in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opcion),
+      );
+
+      //(lista.add(tempWidget);
+      //lista.add(Divider());
+
+      lista..add(tempWidget)
+      ..add(Divider());
+    }
+
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorta() {
+    var widgets = opciones.map((opcion) => 
+    ListTile(
+      title: Text(opcion)
+    )).toList();
+
+    return widgets;
   }
   
 }
