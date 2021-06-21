eabc6ceee26b3f024d8a4a56ec800abc55f5757f
Mitchell Rodríguez <mitrodle@gmail.com>
2021-06-21 13:33:10 -0500

    [Add]: HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:componentes/src/pages/home_temp.dart';
 import 'package:flutter/material.dart';
+import 'package:componentes/src/pages/home_page.dart';
  
 void main() => runApp(MyApp());
  
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes App',
       debugShowCheckedModeBanner: false,
-      home: HomePageTemp()
+      home: HomePage()
     );
   }
 }
