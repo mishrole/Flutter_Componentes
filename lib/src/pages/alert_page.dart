43957f708d2b26b0f7ea0ca21b590407c5c677d4
Mitchell Rodríguez <mitrodle@gmail.com>
2021-06-22 05:09:25 -0500

    [Add]: Alert + Elevated Button

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -5,7 +5,17 @@ class AlertPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Alert Page')),
+      appBar: AppBar(
+        title: Text('Alert Page')
+      ),
+      body: Center(
+        child: ElevatedButton(child: Text('Mostrar Alerta'),
+        onPressed: () => _mostrarAlert(context),
+        style: ElevatedButton.styleFrom(
+          primary: Colors.red,
+          shape: StadiumBorder()
+        )),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_left),
         onPressed: () {
@@ -13,5 +23,25 @@ class AlertPage extends StatelessWidget {
         }
       ),
     );
+  }
+
+  void _mostrarAlert(BuildContext context) {
+    showDialog(context: context, builder: (context) {
+      return AlertDialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+        title: Text('Título'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text('Este es el contenido de la caja'),
+            FlutterLogo(size: 100.0)
+          ],
+        ),
+        actions: <Widget>[
+          ElevatedButton(onPressed: () => Navigator.of(context).pop(), child: Text('Cancelar')),
+          ElevatedButton(onPressed: () => Navigator.of(context).pop(), child: Text('Aceptar')),
+        ],
+      );
+    });
   }
 }
