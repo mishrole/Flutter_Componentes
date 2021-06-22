e2ca0edef4321a2b97390a5a3c0ff818561e3493
Mitchell Rodríguez <mitrodle@gmail.com>
2021-06-22 13:30:58 -0500

    [Add]: Circle Avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
 
 class AvatarPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Avatar Page'))
-    );
+        appBar: AppBar(title: Text('Avatar Page'), actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage:
+                  NetworkImage('https://boredhumans.b-cdn.net/faces2/505.jpg'),
+              radius: 30.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.orange,
+            ),
+          )
+        ]),
+        body: Center(
+          child: FadeInImage(
+            image: NetworkImage('https://boredhumans.b-cdn.net/faces2/505.jpg'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            fadeInDuration: Duration(microseconds: 200),
+          ),
+        ));
   }
 }
