462b650119b82537f6c83dfb547746d4827a5c90
Mitchell Rodríguez <mitrodle@gmail.com>
2021-06-21 19:20:36 -0500

    [Fix]: Border Card -> Container

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -17,50 +17,71 @@ class CardPage extends StatelessWidget {
       ),
     );
   }
-}
 
-Widget _cardTipo1() {
-  return Card(
-    child: Column(children: [
-      ListTile(
-        leading: Icon(Icons.photo_album, color: Colors.blue),
-        title: Text('Card'),
-        subtitle: Text('Esta es la descripción de la tarjeta con muchísimo contenido'),
-      ),
-      Row(
-        children: <Widget>[
-          TextButton(child: Text('Cancelar'),
-            onPressed: () {},
-          ),
-          TextButton(child: Text('Aceptar'),
-            onPressed: () {},
-          ),
-        ],
-        mainAxisAlignment: MainAxisAlignment.end,
-      )
-    ],)
-  );
-}
-
-Widget _cardTipo2() {
-  return Card(
-    child: Column(
-      children: <Widget>[
-        FadeInImage(
-          placeholder: AssetImage('assets/jar-loading.gif'),
-          image: NetworkImage('https://th.bing.com/th/id/OIP.9ftNIxFdXhhw1jqMYwDhMQHaEo?pid=ImgDet&rs=1'),
-          fadeInDuration: Duration( milliseconds: 200 ),
-          height: 250.0,
-          fit: BoxFit.cover,
-          ),
-        /*
-        Image(
-          image: NetworkImage('https://th.bing.com/th/id/OIP.9ftNIxFdXhhw1jqMYwDhMQHaEo?pid=ImgDet&rs=1')
+  Widget _cardTipo1() {
+    return Card(
+      elevation: 5.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      child: Column(children: [
+        ListTile(
+          leading: Icon(Icons.photo_album, color: Colors.blue),
+          title: Text('Card'),
+          subtitle: Text('Esta es la descripción de la tarjeta con muchísimo contenido'),
         ),
-        */
-        Container(child: Text('Hola'),
-        padding: EdgeInsets.all(10.0),)
-      ],
-    )
-  );
+        Row(
+          children: <Widget>[
+            TextButton(child: Text('Cancelar'),
+              onPressed: () {},
+            ),
+            TextButton(child: Text('Aceptar'),
+              onPressed: () {},
+            ),
+          ],
+          mainAxisAlignment: MainAxisAlignment.end,
+        )
+      ],)
+    );
+  }
+
+  Widget _cardTipo2() {
+    final card = Container(
+      // clipBehavior: Clip.antiAlias,
+      child: Column(
+        children: <Widget>[
+          FadeInImage(
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            image: NetworkImage('https://th.bing.com/th/id/OIP.9ftNIxFdXhhw1jqMYwDhMQHaEo?pid=ImgDet&rs=1'),
+            fadeInDuration: Duration( milliseconds: 200 ),
+            height: 250.0,
+            fit: BoxFit.cover,
+            ),
+          /*
+          Image(
+            image: NetworkImage('https://th.bing.com/th/id/OIP.9ftNIxFdXhhw1jqMYwDhMQHaEo?pid=ImgDet&rs=1')
+          ),
+          */
+          Container(
+            child: Text('Hola'),
+            padding: EdgeInsets.all(10.0),)
+        ],
+      )
+    );
+
+    return Container(
+      child: ClipRRect(child: card, borderRadius: BorderRadius.circular(20.0),),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        color: Colors.white,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10.0,
+            spreadRadius: 2.0,
+            offset: Offset(2.0, 10.0)
+          )
+        ],
+      )
+    );
+  }
 }
+
