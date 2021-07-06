8a7ab23d38e481d5693123823047cd2afbb5720d
Mitchell Rodríguez <mitrodle@gmail.com>
2021-07-06 01:54:20 -0500

    [Add]: Email + Password Input

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -10,16 +10,21 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
 
   String _nombre = '';
+  String _email = '';
+  String _password = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Inputs Container'),),
-      // body: Center(child: Container(
         body: ListView(
           padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
           children: <Widget>[
             _crearInput(),
+            Divider(),
+            _crearEmail(),
+            Divider(),
+            _crearPassword(),
             Divider(),
             _crearPersona(),
           ],
@@ -29,7 +34,6 @@ class _InputPageState extends State<InputPage> {
 
   Widget _crearInput() {
     return TextField(
-      // textCapitalization: TextCapitalization.words,
       textCapitalization: TextCapitalization.sentences,
       decoration: InputDecoration(
         border: OutlineInputBorder(
@@ -42,17 +46,46 @@ class _InputPageState extends State<InputPage> {
         suffixIcon: Icon(Icons.accessibility),
         icon: Icon(Icons.account_circle)
       ),
-      onChanged: (value) {
-        setState(() {
-          _nombre = value;
-        });
-      },
+      onChanged: (value) => setState(() { _nombre = value; }),
     );
   }
 
-  _crearPersona() {
+  Widget _crearPersona() {
     return ListTile(
       title: Text('Nombre es: $_nombre'),
+      subtitle: Text('Email es: $_email y contraseña es: $_password')
+    );
+  }
+
+  Widget _crearEmail() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        hintText: 'Email de la persona',
+        labelText: 'Email',
+        suffixIcon: Icon(Icons.alternate_email),
+        icon: Icon(Icons.email)
+      ),
+      onChanged: (value) => setState(() { _email = value; }),
+    );
+  }
+
+  Widget _crearPassword() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        hintText: 'Password',
+        labelText: 'Password',
+        suffixIcon: Icon(Icons.lock_open),
+        icon: Icon(Icons.lock)
+      ),
+      onChanged: (value) => setState(() { _password = value; }),
     );
   }
 }
